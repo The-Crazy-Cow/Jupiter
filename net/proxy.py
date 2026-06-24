454c675eab5d650de79d7b7ac99845849d487ad7
alice <alice@vbox.myguest.virtualbox.org>
2026-06-24 22:04:26 +0000

    add the server init

---
+++ net/proxy.py
@@ -0,0 +1,57 @@
+#tcp procy
+
+import sys
+import socket
+import ssl
+import threading
+import  argparse
+import  textwrap
+
+#const
+MAX_PROXY_LISTEN=5
+
+def server_loop(
+    local_host: str,
+    local_port: int,
+    remote_host: str,
+    remote_port: int,
+    receive_first: bool,
+    ssl_client: bool,
+    ssl_remote: bool,
+    no_verify: bool,
+    certfile: str,
+    keyfile: str,
+) -> None:
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    try:
+        server.bind((local_host, local_port))
+    except OSError as exc:
+        print(f"[!!] Failed to bind {local_host}:{local_port} — {exc}")
+        sys.exit(1)
+
+    
+    print(f"[*] Listening on {local_host}:{local_port}")
+    print(f"[*] Forwarding to {remote_host}:{remote_port}")
+    print(f"[*] Client-side SSL : {'ON' if ssl_client else 'off'}")
+    print(f"[*] Remote-side SSL : {'ON' if ssl_remote else 'off'}")
+    server.listen(MAX_PROXY_LISTEN)
+
+    while True:
+        client_raw,addr =  server.accept()
+        print(f"\n[*] Connection from {addr[0]}:{addr[1]}")
+
+        client_sock: socket.socket = client_raw
+
+        pass
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
