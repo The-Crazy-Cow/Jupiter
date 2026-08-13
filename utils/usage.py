80c2c9500367f44b4f43dcc094ed8adaaf4411d5
alice <jdj17180@gmail.com>
2026-08-13 05:12:48 +0200

    utils/usage - fix the argv

--- utils/usage.py
+++ utils/usage.py
@@ -4,32 +4,37 @@ import argparse
 import sys
 
 class Usage:
-    def __init__(self,description:str,add_help=True)->None:
+
+    def __init__(self, description: str, add_help=True) -> None:
         if not description:
             raise ValueError("empty 'description'")
-        self.__parser = argparse.ArgumentParser(description=description,add_help=add_help)
+
+        self.__parser = argparse.ArgumentParser(
+            description=description,
+            add_help=add_help
+        )
 
     @property
     def parser(self):
         return self.__parser
 
     @parser.setter
-    def parser(self,parser:argparse.ArgumentParser):
-        if not isinstance(parser,argparse.ArgumentParser):
-            f"parser must be an ArgumentParser, got {type(parser)}"
-        self.__parser=parser
+    def parser(self, parser: argparse.ArgumentParser):
+        if not isinstance(parser, argparse.ArgumentParser):
+            raise TypeError(
+                f"parser must be an ArgumentParser, got {type(parser)}"
+            )
 
-    def parse(self,argv):
+        self.__parser = parser
+
+    def parse(self, argv=None):
         if argv is None:
             argv = sys.argv[1:]
-        else:
-            self.__parser.print_help()
-            return
-            
-        args, _ = self.__parser.parse_known_args()
 
-        if getattr(args,"help",False):
+        args, _ = self.__parser.parse_known_args(argv)
+
+        if getattr(args, "help", False):
             self.__parser.print_help()
             return None
 
-        args = self.__parser.parse_args(argv)
+        return self.__parser.parse_args(argv)
